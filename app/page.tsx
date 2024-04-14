1146442a311f6e64eeff844df19599d55bf0fb37
Michael <mvelasco@wasdstudio.com>
2024-04-14 01:17:26 -0600

    Form implementation

--- app/page.tsx
+++ app/page.tsx
@@ -1,112 +1,141 @@
 import Image from "next/image";
+import React,{useState, ChangeEvent, FormEvent} from "react";
+
+type FormData = {
+  projectName: string;
+  projectDescription: string;
+  monthlyContribution: string;
+  donationPercentage: number;
+  idNumber: string;
+  phone: string;
+  email: string;
+  productImage: File | null;
+};
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between p-5  bg-indigo-900">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
+        
+        <div className="fixed bottom-0 left-0 flex h-48 w-full justify-center lg:static lg:size-auto lg:bg-none text-center items-center">
             <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
+              src="/bfree.png"
+              alt="B.free Logo"
               width={100}
-              height={24}
+              height={100}
               priority
-            />
-          </a>
+            />          
+            <h1 className="text-3xl">B.free</h1>            
         </div>
-      </div>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+        
+      </div>
+      <div>
+      <form  className="p-8">
+      <div className="mb-4">
+        <label htmlFor="projectName" className="block text-sm font-medium text-white">
+          NOMBRE DE TU PROYECTO
+        </label>
+        <input
+          type="text"
+          name="projectName"
+          id="projectName"          
+          className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm p-2 text-black"
+          required
         />
       </div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+      <div className="mb-4">
+        <label htmlFor="projectDescription" className="block text-sm font-medium text-white">
+          EXPLICA CLARAMENTE DE QUÉ SE TRATA
+        </label>
+        <textarea
+          name="projectDescription"
+          id="projectDescription"          
+          rows = "4" 
+          className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm p-2 text-black"
+          required
+        ></textarea>
+      </div>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div className="mb-4">
+        <label className="block text-sm font-medium text-white">
+          CON CUÁNTO DINERO POR MES PUEDES PARTICIPAR
+        </label>
+        <select
+          name="monthlyContribution"
+          className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm p-2 text-black"
+          required
         >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+          <option value="">Selecciona una opción</option>
+          <option value="500">500 pesos por mes</option>
+          <option value="1000">1000 pesos por mes</option>
+        </select>
+      </div>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+      <div className="mb-4">
+        <label htmlFor="donationPercentage" className="block text-sm font-medium text-white">
+          CUÁL ES EL PORCENTAJE DE TU GANANCIA QUE DONARÁS A B.FREE (mínimo 20% máximo lo que tú decidas)
+        </label>
+        <input
+          type="number"
+          name="donationPercentage"
+          id="donationPercentage"
+          min="20"
+          max="100"          
+          className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm p-2 text-black"
+          required
+        />
+      </div>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className="mb-4">
+        <label className="block text-sm font-medium text-white">
+          DEJA TUS DATOS DE CONTACTO:
+        </label>
+        <input
+          type="text"
+          name="idNumber"
+          placeholder="No. de INE"
+          
+          className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm p-2 mb-2 text-black"
+          required
+        />
+        <input
+          type="tel"
+          name="phone"
+          placeholder="Teléfono"
+          
+          className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm p-2 mb-2 text-black"
+          required
+        />
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          
+          className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm p-2 text-black"
+          required
+        />
+      </div>
+
+      <div className="mb-4">
+        <label htmlFor="productImage" className="block text-sm font-medium text-white">
+          SUBE UNA FOTO DEL PRODUCTO QUE VENDERÁS
+        </label>
+        <input
+          type="file"
+          name="productImage"
+          id="productImage"
+          
+          className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm p-2"
+          required
+        />
+      </div>
+
+      <button type="submit" className="w-full bg-button-color text-white rounded-md p-2">
+        Enviar
+      </button>
+    </form>
       </div>
     </main>
   );
